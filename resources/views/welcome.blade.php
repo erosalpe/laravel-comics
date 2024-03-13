e5f5094894859e50bc82a3281e729c0148b48aa4
erosalpe <eros.alpegiani@gmail.com>
2024-03-13 15:40:53 +0100

    feat(welcome.php) aggiunta sezione icone sfondo blu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,6 +72,32 @@
             top: -35px;
             left: -30px;
         }
+
+        #blueIconsBg{
+        background-color: #0c7cec;
+        div{
+            ul{
+                list-style-type: none;
+                li{
+                    a{
+                        text-decoration: none;
+                        color: white;
+                        font-size: 0.8rem;
+                        img{
+                            width: 25%;
+                        }
+                    }
+                    &:nth-child(4){
+                        a{
+                            img{
+                                width: 15%;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        }
     </style>
 @endsection
 
@@ -106,6 +132,43 @@
         @endforeach
             <button class="m-auto px-5 py-2 text-light bg-primary border-0 fw-bold">Load More</button>
             <h4 class="text-uppercase px-5 py-3 text-light bg-primary fw-bold position-absolute" id="currentSeriesBanner">current series</h4>
+        </div>
+    </div>
+
+    <div id="blueIconsBg">
+        <div class="w-75 m-auto py-5 container-fluid">
+            <ul class="d-flex m-0 justify-content-between row">
+                <li class="col-2">
+                    <a href="#" class="d-flex align-items-center gap-2">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="menuIcon">
+                        <span class="text-uppercase">digital comics</span>
+                    </a>
+                </li>
+                <li class="col-2">
+                    <a href="#" class="d-flex align-items-center gap-2">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="menuIcon">
+                        <span class="text-uppercase">dc merchandise</span>
+                    </a>
+                </li>
+                <li class="col-2">
+                    <a href="#" class="d-flex align-items-center gap-2">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="menuIcon">
+                        <span class="text-uppercase">subscription</span>
+                    </a>
+                </li>
+                <li class="col-2">
+                    <a href="#" class="d-flex align-items-center gap-2">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="menuIcon">
+                        <span class="text-uppercase">comic shop locator</span>
+                    </a>
+                </li>
+                <li class="col-2">
+                    <a href="#" class="d-flex gap-2 justify-content-center align-items-center p-2">
+                        <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="menuIcon">
+                        <span class="text-uppercase">dc power visa</span>
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
 @endsection
